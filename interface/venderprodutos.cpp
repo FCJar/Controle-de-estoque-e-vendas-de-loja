e22b44290e0d1f0ed38b2f13ae9be4451301eaae
brunomprk <130181376+brunomprk@users.noreply.github.com>
2023-12-04 20:57:28 -0300

    refatorando venderprodutos.cpp

--- interface/venderprodutos.cpp
+++ interface/venderprodutos.cpp
@@ -1,11 +1,8 @@
 #include "venderprodutos.h"
 #include "ui_venderprodutos.h"
-#include <string>
 #include <iostream>
 #include <QMessageBox>
-#include <exception>
-
-
+#include <cassert>
 
 VenderProdutos::VenderProdutos(QWidget *parent) :
     QDialog(parent),
@@ -14,10 +11,11 @@ VenderProdutos::VenderProdutos(QWidget *parent) :
     ui->setupUi(this);
 }
 
-void VenderProdutos::setDados(Estoque* e,Caixa* c,std::vector<Funcionario*> *f){
-    e0=e;
-    cx0=c;
-    f0=f;
+void VenderProdutos::setDados(Estoque* e, Caixa* c, std::vector<Funcionario*> *f)
+{
+    e0 = e;
+    cx0 = c;
+    f0 = f;
 }
 
 VenderProdutos::~VenderProdutos()
@@ -25,93 +23,103 @@ VenderProdutos::~VenderProdutos()
     delete ui;
 }
 
-
 void VenderProdutos::on_AdProduto_bt_clicked()
 {
-    std::string nomeP,nome,fp,end,dtV;
-    int qtd,id;
-    try{
-        nomeP=ui->Produto_txt->text().toStdString();
-        qtd=stoi(ui->Qtd_txt->text().toStdString());
-        nome=ui->Nome_txt->text().toStdString();
-        id=stoi(ui->Id_txt->text().toStdString());
-        fp=ui->FPag_txt->text().toStdString();
-        end=ui->End_txt->text().toStdString();
-        dtV=ui->Data_txt->text().toStdString();
+    try {
+        std::string nomeProduto, nomeFuncionario, formaPagamento, endereco, dataVenda;
+        int quantidade, idFuncionario;
 
-        // Adiciona asserts para verificar as condições
-        assert(qtd >= 0.0 && "A quantidade não pode ser negativo");
-        assert(id >= 0.0 && "O id não pode ser negativo");
-        assert(!nome.empty() && "O nome não pode estar vazio");
-        assert(!nomeP.empty() && "O nome do produto não pode estar vazio");
+        nomeProduto = ui->Produto_txt->text().toStdString();
+        quantidade = stoi(ui->Qtd_txt->text().toStdString());
+        nomeFuncionario = ui->Nome_txt->text().toStdString();
+        idFuncionario = stoi(ui->Id_txt->text().toStdString());
+        formaPagamento = ui->FPag_txt->text().toStdString();
+        endereco = ui->End_txt->text().toStdString();
+        dataVenda = ui->Data_txt->text().toStdString();
 
-    }catch(const std::invalid_argument& e){
-        QMessageBox::critical(this,"Mensagem","Dados de produto invalido");
+        assert(quantidade >= 0 && "A quantidade não pode ser negativa");
+        assert(idFuncionario >= 0 && "O ID não pode ser negativo");
+        assert(!nomeFuncionario.empty() && "O nome não pode estar vazio");
+        assert(!nomeProduto.empty() && "O nome do produto não pode estar vazio");
+    } catch (const std::invalid_argument& e) {
+        QMessageBox::critical(this, "Mensagem", "Dados de produto inválido");
+        return;
     }
 
-    try{
-        bool aux=true;
-        for(auto (it)=f0->begin();(it)!=f0->end();it++){
-            if((*it)->getnomeCompleto()==nome && (*it)->getId()==id && pedidoInicializado==false){
-                (*it)->criarPedido(fp,end,dtV);
-                (*it)->venderProduto(e0,nomeP,qtd);
-                pedidoInicializado==true;
-                std::string auxMsg="Produto adcionado: ";
-                auxMsg+=nomeP;
-                auxMsg+=" Quantidade: ";
-                auxMsg+=qtd;
+    try {
+        bool encontrado = false;
 
-                QString msg=QString::fromStdString(auxMsg);
-                QMessageBox::about(this,"Mensagem",msg);
-                aux=false;
-            }else if((*it)->getnomeCompleto()==nome && (*it)->getId()==id && pedidoInicializado==true){
-                (*it)->venderProduto(e0,nomeP,qtd);
-                aux=false;
+        for (auto it = f0->begin(); it != f0->end(); it++) {
+            std::string nomeProduto, nomeFuncionario, formaPagamento, endereco, dataVenda;
+            int quantidade, idFuncionario;
+
+            nomeProduto = ui->Produto_txt->text().toStdString();
+            quantidade = stoi(ui->Qtd_txt->text().toStdString());
+            nomeFuncionario = ui->Nome_txt->text().toStdString();
+            idFuncionario = stoi(ui->Id_txt->text().toStdString());
+            formaPagamento = ui->FPag_txt->text().toStdString();
+            endereco = ui->End_txt->text().toStdString();
+            dataVenda = ui->Data_txt->text().toStdString();
+
+            if ((*it)->getnomeCompleto() == nomeFuncionario && (*it)->getId() == idFuncionario && !pedidoInicializado) {
+                (*it)->criarPedido(formaPagamento, endereco, dataVenda);
+                (*it)->venderProduto(e0, nomeProduto, quantidade);
+                pedidoInicializado = true;
+
+                QString msg = QString("Produto adicionado: %1 Quantidade: %2").arg(QString::fromStdString(nomeProduto)).arg(quantidade);
+                QMessageBox::about(this, "Mensagem", msg);
+
+                encontrado = true;
+                break;
+            } else if ((*it)->getnomeCompleto() == nomeFuncionario && (*it)->getId() == idFuncionario && pedidoInicializado) {
+                (*it)->venderProduto(e0, nomeProduto, quantidade);
+                encontrado = true;
+                break;
             }
         }
-        if(aux==true){
-            throw std::invalid_argument("Produto não encontrado ou funcionario invalido");
+
+        if (!encontrado) {
+            throw std::invalid_argument("Produto não encontrado ou funcionário inválido");
         }
-    }catch(const std::invalid_argument& e){
-        QMessageBox::critical(this,"Mensagem",e.what());
+    } catch (const std::invalid_argument& e) {
+        QMessageBox::critical(this, "Mensagem", e.what());
     }
+
     ui->Produto_txt->clear();
     ui->Qtd_txt->clear();
-
 }
-
 
 void VenderProdutos::on_FinalizaP_bt_clicked()
 {
+    try {
+        std::string nomeFuncionario;
+        int idFuncionario;
 
-    std::string nome;
-    int id;
+        nomeFuncionario = ui->Nome_txt->text().toStdString();
+        idFuncionario = stoi(ui->Id_txt->text().toStdString());
 
-    try{
-        nome=ui->Nome_txt->text().toStdString();
-        id=stoi(ui->Id_txt->text().toStdString());
+        if (!pedidoInicializado) {
+            throw std::invalid_argument("Pedido não foi inicializado");
+        }
 
-        bool aux=true;
+        bool encontrado = false;
 
-        if(pedidoInicializado==false){
-            throw std::invalid_argument("Produtos não foi inicializado");
-        }else{
-            for(auto it=f0->begin();it!=f0->end();it++){
-                if((*it)->getnomeCompleto()==nome && (*it)->getId()==id && pedidoInicializado==true){
-                    (*it)->finalizarPedido(cx0);
-                    QMessageBox::about(this,"Mensagem","Compra finalizada");
-                    pedidoInicializado=false;
-                    aux=false;
-                }
+        for (auto it = f0->begin(); it != f0->end(); it++) {
+            if ((*it)->getnomeCompleto() == nomeFuncionario && (*it)->getId() == idFuncionario) {
+                (*it)->finalizarPedido(cx0);
+                QMessageBox::about(this, "Mensagem", "Compra finalizada");
+                pedidoInicializado = false;
+
+                encontrado = true;
+                break;
             }
         }
 
-        if(aux){
-            throw std::invalid_argument("Funcionario não foi encontrado");
+        if (!encontrado) {
+            throw std::invalid_argument("Funcionário não foi encontrado");
         }
-
-    }catch(const std::invalid_argument& e){
-        QMessageBox::critical(this,"Mensagem",e.what());
+    } catch (const std::invalid_argument& e) {
+        QMessageBox::critical(this, "Mensagem", e.what());
     }
 
     ui->Produto_txt->clear();
@@ -121,6 +129,4 @@ void VenderProdutos::on_FinalizaP_bt_clicked()
     ui->FPag_txt->clear();
     ui->End_txt->clear();
     ui->Data_txt->clear();
-
-
 }
