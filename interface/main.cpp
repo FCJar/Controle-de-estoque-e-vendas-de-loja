8cceb9dd1fa4fa700780ebb3be231a3f527df104
FCJar <Fcjardim764@gmail.com>
2023-12-03 22:14:54 -0300

    1.3.6

--- interface/main.cpp
+++ interface/main.cpp
@@ -13,8 +13,12 @@ int main(int argc, char *argv[])
     MainWindow w;
     Estoque *e0=new Estoque(1);
     Caixa *cx=new Caixa(1);
-    std::vector<Funcionario*> *funcs;
-    std::vector<Gerente*> *g0;
+    std::vector<Funcionario*> *funcs = new std::vector<Funcionario*>;
+    std::vector<Gerente*> *g0 = new std::vector<Gerente*>;
+    Gerente* gaux=new Gerente(123456,1,"121212","Sergio",20000);
+    g0->push_back(gaux);
+    Funcionario *faux=new Funcionario(2,"1456456-66","Carlos",1000.00);
+    funcs->push_back(faux);
     Produto* p0=new Produto(1,"Pastel","Comida",50000.0,200.0,5);
     Produto* p1=new Produto(2,"Carvão","equipamento",75000.0,200.0,5);
     Produto* p2=new Produto(3,"melancia","Comida",60000.0,200.0,5);
@@ -23,6 +27,9 @@ int main(int argc, char *argv[])
     e0->adcionarProduto(p1);
     e0->adcionarProduto(p2);
     e0->adcionarProduto(p3);
+    faux->criarPedido("A vista","Rua exemplo","Algum dia");
+    faux->venderProduto(e0,"Pastel",1);
+    faux->finalizarPedido(cx);
     w.SetDados(e0,cx,funcs,g0);
     w.show();
     return a.exec();
