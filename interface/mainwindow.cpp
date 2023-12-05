5bd073eac33aa26698860af004cc830eda2b6014
brunomprk <130181376+brunomprk@users.noreply.github.com>
2023-12-04 21:02:57 -0300

    refatorando mainwindow

--- interface/mainwindow.cpp
+++ interface/mainwindow.cpp
@@ -1,8 +1,9 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    : QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 }
@@ -14,55 +15,50 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_AdProduto_clicked()
 {
-    AdProduto ap;
-    ap.setEstoque(e0,g0);
-    ap.exec();
+    AdProduto adProdutoDialog;
+    adProdutoDialog.setEstoque(e0, g0);
+    adProdutoDialog.exec();
 }
-
 
 void MainWindow::on_EEstoque_clicked()
 {
-    ExibirEstoque exb;
-    exb.setDados(e0);
-    exb.exec();
+    ExibirEstoque exibirEstoqueDialog;
+    exibirEstoqueDialog.setDados(e0);
+    exibirEstoqueDialog.exec();
 }
-
 
 void MainWindow::on_AdFunc_clicked()
 {
-    AdcionarFuncionarios ad;
-    ad.setDados(funcs);
-    ad.exec();
+    AdcionarFuncionarios adicionarFuncionariosDialog;
+    adicionarFuncionariosDialog.setDados(funcs);
+    adicionarFuncionariosDialog.exec();
 }
-
 
 void MainWindow::on_VProduto_clicked()
 {
-    VenderProdutos v;
-    v.setDados(e0,cx,funcs);
-    v.exec();
+    VenderProdutos venderProdutosDialog;
+    venderProdutosDialog.setDados(e0, cx, funcs);
+    venderProdutosDialog.exec();
 }
-
 
 void MainWindow::on_pushButton_clicked()
 {
-    DadosDeVenda d;
-    d.setDados(cx,g0);
-    d.exec();
+    DadosDeVenda dadosDeVendaDialog;
+    dadosDeVendaDialog.setDados(cx, g0);
+    dadosDeVendaDialog.exec();
 }
-
 
 void MainWindow::on_RemoverPro_clicked()
 {
-    RemoveProduto r;
-    r.setDados(e0,funcs);
-    r.exec();
+    RemoveProduto removerProdutoDialog;
+    removerProdutoDialog.setDados(e0, funcs);
+    removerProdutoDialog.exec();
 }
 
-void MainWindow::SetDados(Estoque *e,Caixa *c,std::vector<Funcionario*> *f,std::vector<Gerente*> *g){
-    e0=e;
-    cx=c;
-    funcs=f;
-    g0=g;
+void MainWindow::SetDados(Estoque *e, Caixa *c, std::vector<Funcionario*> *f, std::vector<Gerente*> *g)
+{
+    e0 = e;
+    cx = c;
+    funcs = f;
+    g0 = g;
 }
-
