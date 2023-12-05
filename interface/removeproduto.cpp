540ed5a6a2d4006f2a3d10110fe3eadc88d086c5
brunomprk <130181376+brunomprk@users.noreply.github.com>
2023-12-04 21:00:24 -0300

    refatorando removeprodutos

--- interface/removeproduto.cpp
+++ interface/removeproduto.cpp
@@ -1,6 +1,5 @@
 #include "removeproduto.h"
 #include "ui_removeproduto.h"
-#include <string>
 #include <iostream>
 #include <QMessageBox>
 #include <exception>
@@ -17,64 +16,65 @@ RemoveProduto::~RemoveProduto()
     delete ui;
 }
 
-void RemoveProduto::setDados(Estoque* e,std::vector<Funcionario*> *f ){
-    e0=e;
-    f0=f;
+void RemoveProduto::setDados(Estoque* e, std::vector<Funcionario*> *f)
+{
+    e0 = e;
+    f0 = f;
 }
-
 
 void RemoveProduto::on_Confirmar_bt_clicked()
 {
-    std::string  faux,pnomeAux;
-    int qtdAux,id;
-    int mutex = 0;
+    try {
+        int id = stoi(ui->Id_txt->text().toStdString());
 
-    for(auto it=(e0)->produtos_.begin();it!=(e0)->produtos_.end();it++){
-        id=stoi(ui->Id_txt->text().toStdString());
-        if((*it)->getId() == id)
-            mutex = 1;
-    }
-
-    if(mutex == 0)
-        QMessageBox::about(this,"Mensagem","Id Errado");
-
-    bool aux=true;
-
-    try{
-        faux=ui->Nome_txt->text().toStdString();
-        id=stoi(ui->Id_txt->text().toStdString());
-
-        pnomeAux=ui->Produto_txt->text().toStdString();
-        qtdAux=stoi(ui->Qtd_txt->text().toStdString());
-
-        try{
-            for(auto it=(f0)->begin();it!=(f0)->end();it++){
-
-                if((*it)->getnomeCompleto()==faux && (*it)->getId()==id){
-                    (*it)->removerProduto(e0,pnomeAux,qtdAux);
-                    ui->Produto_txt->text().toStdString();
-                    ui->Qtd_txt->clear();
-                    ui->Produto_txt->clear();
-                    std::cout << "Produto Removido";
-                    QMessageBox::about(this,"Mensagem","Produto Removido");
-                    aux=false;
-                }
+        // Verificar se o ID existe no estoque
+        bool idEncontrado = false;
+        for (const auto& produto : e0->produtos_) {
+            if (produto->getId() == id) {
+                idEncontrado = true;
+                break;
             }
-            if(aux){
-                QMessageBox::about(this,"Mensagem","Funcionário não encontrado");
-                // throw std::invalid_argument("Funcionario não encontrado");
-            }
-        }catch(const std::invalid_argument& e){
-            std::cout << "Catch" << std::endl;
-            // QMessageBox::about(this,"Mensagem",e.what());
         }
 
+        if (!idEncontrado) {
+            QMessageBox::about(this, "Mensagem", "ID Errado");
+            return;
+        }
 
-    }catch(const std::invalid_argument& e){
-        QMessageBox::about(this,"Mensagem","Dados de produto ou funcionario invalido");
+        std::string faux = ui->Nome_txt->text().toStdString();
+        std::string pnomeAux = ui->Produto_txt->text().toStdString();
+        int qtdAux = stoi(ui->Qtd_txt->text().toStdString());
+
+        bool funcionarioEncontrado = false;
+
+        for (auto it = f0->begin(); it != f0->end(); ++it) {
+            if ((*it)->getnomeCompleto() == faux && (*it)->getId() == id) {
+                try {
+                    // Remover o produto associado ao funcionário
+                    (*it)->removerProduto(e0, pnomeAux, qtdAux);
+
+                    // Limpar campos de entrada
+                    ui->Produto_txt->clear();
+                    ui->Qtd_txt->clear();
+
+                    std::cout << "Produto Removido" << std::endl;
+                    QMessageBox::about(this, "Mensagem", "Produto Removido");
+
+                    funcionarioEncontrado = true;
+                    break;
+                } catch (const std::invalid_argument& e) {
+                    // Tratar exceção relacionada à remoção do produto
+                    std::cout << "Catch: " << e.what() << std::endl;
+                    QMessageBox::about(this, "Mensagem", e.what());
+                }
+            }
+        }
+
+        if (!funcionarioEncontrado) {
+            QMessageBox::about(this, "Mensagem", "Funcionário não encontrado");
+        }
+    } catch (const std::invalid_argument& e) {
+        // Tratar exceção relacionada à conversão ou entrada inválida
+        QMessageBox::about(this, "Mensagem", "Dados de produto ou funcionário inválidos");
     }
-
-
-
 }
-
